6f2c858e0a22d8239151c93ad03c000d708ed2f9
AFTAB AHMED <aftabemir123@gmail.com>
2022-10-07 21:02:58 +0530

    supplier functionalites

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,flash
 import pyrebase
 firebaseConfig = {
   "apiKey": "AIzaSyD0KS2N_37H5Pikp3pkAS5BhLkzVqMMIfE",
@@ -113,7 +113,7 @@ def showSuppliers():
 
     return render_template("suppliers.html",allSuppliersDetails=allSuppliersDetails)
 
-current_order_dict={}
+current_order_dict={'total':0}
 def addToDict(itemName,quantity):
 
     current_order_dict[itemName]=quantity
@@ -130,9 +130,82 @@ def recieve(supName):
         print(current_order_dict)
     return render_template("orderRecieving.html",supName=supName,supplierDetails=supplierDetails,current_order_dict=current_order_dict)
 
+def updateCurrentStateOfInventory(ordersDict):
+    #priceList  =[3000,'kg',30] [total price, unit, quanity]
+    #ordersDict['total'] doesnt have the pricelist
+    for itemName,priceList in ordersDict.items():
+        if itemName!='total':
+            itemDBObject=db.child('inventory').child(itemName).get().val() #[30,'kg']
+            if itemDBObject != None:
+                itemDBObject[0]=itemDBObject[0]+priceList[2] #adding current quanytiy and order quanity
+            else:
+                itemDBObject=[]
+                itemDBObject.append(priceList[1])
+                itemDBObject.append(priceList[2])
+            db.child('inventory').update({itemName:itemDBObject}) #updating the item
+
+
+def getMinimumSupplier(item):
+    min_data = [99999]
+    min_sup = 'no supplier'
+    allSuppliers = getDetailsOfAllSuppliers()
+    for supplier, items in allSuppliers.items():
+
+        if item in items:
+            price = items[item][0]
+            if price < min_data[0]:
+                min_data[0] = price
+                min_sup = supplier
+    min_data.append(min_sup)
+    return min_data
+
+def updateTotalWorthInventory():
+    total=0
+    current_state=db.child('inventory').get().val()
+    for item in current_state:
+        if item =='totalWorth' or item == 'issue':
+            continue
+        quantity=current_state[item][0]
+        minPrice=getMinimumSupplier(item)[0]
+        total+=quantity*minPrice
+    print(total)
+    db.child('inventory').update({'totalWorth':total})
+
+
 @app.route("/store/placeOrder/<supName>")
 def placingOrder(supName):
     print("placing orders for "+supName)
+    print(current_order_dict)
+    prices_sup=db.child("supplier").child(supName).get().val()
+    #print(prices_sup)
+    for recieved_item in current_order_dict:
+        if recieved_item in prices_sup:
+            item_price=prices_sup[recieved_item]
+            quantity=current_order_dict[recieved_item]
+            item_price[0]=int(item_price[0])*int(quantity)
+            current_order_dict['total']+=item_price[0]
+            item_price.append(int(quantity))
+            current_order_dict[recieved_item]=item_price
+        else:
+            print(recieved_item+" is not there on the db for this supplier "+supName)
+
+    print('final order list on db \n')
+    print(current_order_dict)
+
+    # placing order for supName with current_order_dict
+
+    db.child("supplier").child(supName).child("orders").push(current_order_dict)
+
+    #updating state of inventory
+
+    updateCurrentStateOfInventory(current_order_dict)
+    updateTotalWorthInventory()
+
+    current_order_dict.clear() #removing the items in dictionary
+    current_order_dict['total']=0
+
+
     return redirect("/store/")
+
 
 app.run(debug=True)
